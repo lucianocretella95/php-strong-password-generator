d39fb90d0fc8b2ad5554b075eaca6807c7c2098b
lucianocretella95 <luciano.cretella@tim.it>
2024-06-12 15:03:23 +0200

    creazione repo

---
+++ scelta.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./scelta.css">
+    <title>Scelta</title>
+</head>
+<body>
+    <h1>Scegli la lunghezza della tua password</h1>
+</body>
+</html>
