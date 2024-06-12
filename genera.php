9c58cac709207b04b801b155fb225d7b872991b5
lucianocretella95 <luciano.cretella@tim.it>
2024-06-12 16:47:00 +0200

    inizio M-2

--- genera.php
+++ genera.php
@@ -7,12 +7,23 @@
     <title>Genera</title>
 </head>
 <body>
+    <?php
+    $caratteri_permessi = [
+        "minuscole" => "q", "w", "e", "r", "t", "y",
+        "maiuscole" => "A", "S", "D", "F", "G", "H",
+        "numeri" => "1", "2", "3", "4", "5", "6",
+        "speciali" => "!", "?", "-", "_", "#", "*",
+    ]
+    ?>
+    <?php  ?>
     <h1>Password generata</h1>
 <?php
     $passlenght = $_GET["name"];
-    for ($x = 0; $x < $passlenght; $x++) {
-        echo "X";
-      }
+    $mypassword = rand(0, $passlenght);
+    for ($i=0; $i < $passlenght ; $i++) {
+        echo $caratteri_permessi['minuscole']. $caratteri_permessi['maiuscole']. $caratteri_permessi['numeri']. $caratteri_permessi['speciali'];
+        echo $mypassword;
+    }
     ?>
 </body>
 </html>
